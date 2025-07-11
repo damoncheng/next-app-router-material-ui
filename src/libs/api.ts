1df3c61cd82bbce4f02da06e28b42ae9e377efa3
damoncheng <354092565@qq.com>
2025-07-11 14:39:59 +0800

    modify env to start with NEXT_PUBLIC_

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.BACKEND_HTTP_URL || 'http://localhost:3000/api',
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_HTTP_URL,
   headers: {
     'Content-Type': 'application/json',
   },
